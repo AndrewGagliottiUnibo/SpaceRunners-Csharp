a471de646a40539fcdd7f3d3680723ec951648bb
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-11-07 19:02:57 +0100

    class done with Nica

---
+++ OOP_Nica/OOP_Nica/OOP_Nica/Model/EntityImpl.cs
@@ -0,0 +1,81 @@
+﻿namespace OOP_Nica.Model
+{
+    public class EntityImpl : IEntity
+    {
+        private int speed;
+        private int horrizontalSpeed;
+        private int positionX;
+        private int positionY;
+        private bool canFire;
+        private string direction;
+
+        public EntityImpl(string direction)
+        {
+            this.direction = direction;
+        }
+
+        public void SetPosition(int x, int y)
+        {
+            this.positionX = x;
+            this.positionY = y;
+        }
+
+        public int GetXPosition()
+        {
+            return this.positionX;
+        }
+
+        public int GetYPosition()
+        {
+            return this.positionY;
+        }
+
+        public int GetSpeed()
+        {
+            return this.speed;
+        }
+
+        public void SetSpeed(int speed)
+        {
+            this.speed = speed;
+        }
+
+        public void SetHorizontalSpeed(int speed)
+        {
+            this.horrizontalSpeed = speed;
+        }
+
+        public int GetHorrizontalSpeed()
+        {
+            return this.horrizontalSpeed;
+        }
+
+        public bool GetCanFire()
+        {
+            return this.canFire;
+        }
+
+        public void SetCanFire(bool canFire)
+        {
+            this.canFire = canFire;
+        }
+
+        /// <summary>
+        /// Change actual direction.
+        /// </summary>
+        /// <param name="newDirection"></param>
+        public void ChangeDirection(string newDirection)
+        {
+            this.direction = newDirection;
+        }
+
+        /// <summary>
+        /// Show actual direction.
+        /// </summary>
+        /// <returns> direction value </returns>
+        public string GetDirection()
+        {
+            return this.direction;
+        }
+    }
+}
---
+++ OOP_Nica/OOP_Nica/OOP_Nica/Model/IEntity.cs
@@ -0,0 +1,51 @@
+﻿namespace OOP_Nica.Model
+{
+    /// <summary>
+    /// Entity interface.
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// Set entity position.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        void SetPosition(int x, int y);
+
+        /// <summary>
+        /// Return position x value
+        /// </summary>
+        /// <returns> x position </returns>
+        int GetXPosition();
+
+        /// <summary>
+        /// Return position y value
+        /// </summary>
+        /// <returns> y position </returns>
+        int GetYPosition();
+
+        /// <summary>
+        /// Show speed value.
+        /// </summary>
+        /// <returns> speed value </returns>
+        int GetSpeed();
+
+        /// <summary>
+        /// Set a speed value.
+        /// </summary>
+        /// <param name="speed"></param>
+        void SetSpeed(int speed);
+
+        /// <summary>
+        /// Set a horizontal speed.
+        /// </summary>
+        /// <param name="speed"></param>
+        void SetHorizontalSpeed(int speed);
+
+        /// <summary>
+        /// Show a speed value.
+        /// </summary>
+        /// <returns> Speed value </returns>
+        int GetHorrizontalSpeed();
+    }
+}
