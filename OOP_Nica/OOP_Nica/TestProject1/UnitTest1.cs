489647494982271baabb0a13aceea35ff548d7a1
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-11-07 19:10:57 +0100

    entity done, test started

--- OOP_Nica/OOP_Nica/TestProject1/UnitTest1.cs
+++ OOP_Nica/OOP_Nica/TestProject1/UnitTest1.cs
@@ -1,13 +1,21 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOP_Nica.Model;
 
 namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+        private IEntity enemy;
+        private IEntity boss;
+        private IEntity player;
+
         [TestMethod]
-        public void TestMethod1()
+        public void EntityGeneralBehaviour()
         {
+            this.enemy = new EntityImpl("naveNemica", "down");
+            this.boss = new EntityImpl("boss", "down");
+            this.player = new EntityImpl("player", "none");
         }
     }
 }
