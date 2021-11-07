c9fda261b45e92b498bf94aadf37612668d02662
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-11-07 18:08:49 +0100

    base for Nica

---
+++ OOP_Nica/OOP_Nica/OOP_Nica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Nica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
